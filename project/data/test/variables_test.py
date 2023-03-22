a0fdb3fb1285bc81e949bbb3b24c2cd8b73884f7
AscCrs <cristoe2003@gmail.com>
2023-03-22 01:47:21 -0600

    Created file for updated data from variables table

---
+++ project/data/test/variables_test.py
@@ -0,0 +1,37 @@
+from project.data.models import Variables
+from project import db
+
+def delete_all_constantes():
+    db.session.query(Variables).delete()
+    db.session.commit()
+
+# Definicion de constantes temporales
+r_pr        = 0.2
+Za          = 0.38
+Zd          = 0.27 
+Zo          = 0.05
+LAI         = 0.0253
+dens_plant  = 542.63
+dens_tierra = 950
+qa          = 0.072
+qf          = dens_tierra / dens_plant
+
+def actVariables():
+    delete_all_constantes()
+    # Verificar si los datos ya existen en la tabla Constantes
+    if db.session.query(Variables).count() == 0:
+        # Agregar los datos a la tabla
+        variables = Variables(
+            r'', 
+            Za, 
+            Zd, 
+            Zo,
+            LAI, 
+            qf,
+            qa
+        )
+        db.session.add(variables)
+        it += 1;
+        db.session.commit()
+    else:
+        print('Elemento duplicado')
